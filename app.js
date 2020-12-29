d31527bbe0b1a3ed8207e7a9864d525753167da2
Venkat <gadiyakarivenkat@gmail.com>
2020-12-29 18:26:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,373 @@
+var countries = {
+    AF: 'Afghanistan',
+    AX: 'Aland Islands',
+    AL: 'Albania',
+    DZ: 'Algeria',
+    AS: 'American Samoa',
+    AD: 'Andorra',
+    AO: 'Angola',
+    AI: 'Anguilla',
+    AQ: 'Antarctica',
+    AG: 'Antigua And Barbuda',
+    AR: 'Argentina',
+    AM: 'Armenia',
+    AW: 'Aruba',
+    AU: 'Australia',
+    AT: 'Austria',
+    AZ: 'Azerbaijan',
+    BS: 'Bahamas',
+    BH: 'Bahrain',
+    BD: 'Bangladesh',
+    BB: 'Barbados',
+    BY: 'Belarus',
+    BE: 'Belgium',
+    BZ: 'Belize',
+    BJ: 'Benin',
+    BM: 'Bermuda',
+    BT: 'Bhutan',
+    BO: 'Bolivia',
+    BA: 'Bosnia And Herzegovina',
+    BW: 'Botswana',
+    BV: 'Bouvet Island',
+    BR: 'Brazil',
+    IO: 'British Indian Ocean Territory',
+    BN: 'Brunei Darussalam',
+    BG: 'Bulgaria',
+    BF: 'Burkina Faso',
+    BI: 'Burundi',
+    KH: 'Cambodia',
+    CM: 'Cameroon',
+    CA: 'Canada',
+    CV: 'Cape Verde',
+    KY: 'Cayman Islands',
+    CF: 'Central African Republic',
+    TD: 'Chad',
+    CL: 'Chile',
+    CN: 'China',
+    CX: 'Christmas Island',
+    CC: 'Cocos (Keeling) Islands',
+    CO: 'Colombia',
+    KM: 'Comoros',
+    CG: 'Congo',
+    CD: 'Congo, Democratic Republic',
+    CK: 'Cook Islands',
+    CR: 'Costa Rica',
+    CI: 'Cote D\'Ivoire',
+    HR: 'Croatia',
+    CU: 'Cuba',
+    CY: 'Cyprus',
+    CZ: 'Czech Republic',
+    DK: 'Denmark',
+    DJ: 'Djibouti',
+    DM: 'Dominica',
+    DO: 'Dominican Republic',
+    EC: 'Ecuador',
+    EG: 'Egypt',
+    SV: 'El Salvador',
+    GQ: 'Equatorial Guinea',
+    ER: 'Eritrea',
+    EE: 'Estonia',
+    ET: 'Ethiopia',
+    FK: 'Falkland Islands (Malvinas)',
+    FO: 'Faroe Islands',
+    FJ: 'Fiji',
+    FI: 'Finland',
+    FR: 'France',
+    GF: 'French Guiana',
+    PF: 'French Polynesia',
+    TF: 'French Southern Territories',
+    GA: 'Gabon',
+    GM: 'Gambia',
+    GE: 'Georgia',
+    DE: 'Germany',
+    GH: 'Ghana',
+    GI: 'Gibraltar',
+    GR: 'Greece',
+    GL: 'Greenland',
+    GD: 'Grenada',
+    GP: 'Guadeloupe',
+    GU: 'Guam',
+    GT: 'Guatemala',
+    GG: 'Guernsey',
+    GN: 'Guinea',
+    GW: 'Guinea-Bissau',
+    GY: 'Guyana',
+    HT: 'Haiti',
+    HM: 'Heard Island & Mcdonald Islands',
+    VA: 'Holy See (Vatican City State)',
+    HN: 'Honduras',
+    HK: 'Hong Kong',
+    HU: 'Hungary',
+    IS: 'Iceland',
+    IN: 'India',
+    ID: 'Indonesia',
+    IR: 'Iran, Islamic Republic Of',
+    IQ: 'Iraq',
+    IE: 'Ireland',
+    IM: 'Isle Of Man',
+    IL: 'Israel',
+    IT: 'Italy',
+    JM: 'Jamaica',
+    JP: 'Japan',
+    JE: 'Jersey',
+    JO: 'Jordan',
+    KZ: 'Kazakhstan',
+    KE: 'Kenya',
+    KI: 'Kiribati',
+    KR: 'Korea',
+    KW: 'Kuwait',
+    KG: 'Kyrgyzstan',
+    LA: 'Lao People\'s Democratic Republic',
+    LV: 'Latvia',
+    LB: 'Lebanon',
+    LS: 'Lesotho',
+    LR: 'Liberia',
+    LY: 'Libyan Arab Jamahiriya',
+    LI: 'Liechtenstein',
+    LT: 'Lithuania',
+    LU: 'Luxembourg',
+    MO: 'Macao',
+    MK: 'Macedonia',
+    MG: 'Madagascar',
+    MW: 'Malawi',
+    MY: 'Malaysia',
+    MV: 'Maldives',
+    ML: 'Mali',
+    MT: 'Malta',
+    MH: 'Marshall Islands',
+    MQ: 'Martinique',
+    MR: 'Mauritania',
+    MU: 'Mauritius',
+    YT: 'Mayotte',
+    MX: 'Mexico',
+    FM: 'Micronesia, Federated States Of',
+    MD: 'Moldova',
+    MC: 'Monaco',
+    MN: 'Mongolia',
+    ME: 'Montenegro',
+    MS: 'Montserrat',
+    MA: 'Morocco',
+    MZ: 'Mozambique',
+    MM: 'Myanmar',
+    NA: 'Namibia',
+    NR: 'Nauru',
+    NP: 'Nepal',
+    NL: 'Netherlands',
+    AN: 'Netherlands Antilles',
+    NC: 'New Caledonia',
+    NZ: 'New Zealand',
+    NI: 'Nicaragua',
+    NE: 'Niger',
+    NG: 'Nigeria',
+    NU: 'Niue',
+    NF: 'Norfolk Island',
+    MP: 'Northern Mariana Islands',
+    NO: 'Norway',
+    OM: 'Oman',
+    PK: 'Pakistan',
+    PW: 'Palau',
+    PS: 'Palestinian Territory, Occupied',
+    PA: 'Panama',
+    PG: 'Papua New Guinea',
+    PY: 'Paraguay',
+    PE: 'Peru',
+    PH: 'Philippines',
+    PN: 'Pitcairn',
+    PL: 'Poland',
+    PT: 'Portugal',
+    PR: 'Puerto Rico',
+    QA: 'Qatar',
+    RE: 'Reunion',
+    RO: 'Romania',
+    RU: 'Russian Federation',
+    RW: 'Rwanda',
+    BL: 'Saint Barthelemy',
+    SH: 'Saint Helena',
+    KN: 'Saint Kitts And Nevis',
+    LC: 'Saint Lucia',
+    MF: 'Saint Martin',
+    PM: 'Saint Pierre And Miquelon',
+    VC: 'Saint Vincent And Grenadines',
+    WS: 'Samoa',
+    SM: 'San Marino',
+    ST: 'Sao Tome And Principe',
+    SA: 'Saudi Arabia',
+    SN: 'Senegal',
+    RS: 'Serbia',
+    SC: 'Seychelles',
+    SL: 'Sierra Leone',
+    SG: 'Singapore',
+    SK: 'Slovakia',
+    SI: 'Slovenia',
+    SB: 'Solomon Islands',
+    SO: 'Somalia',
+    ZA: 'South Africa',
+    GS: 'South Georgia And Sandwich Isl.',
+    ES: 'Spain',
+    LK: 'Sri Lanka',
+    SD: 'Sudan',
+    SR: 'Suriname',
+    SJ: 'Svalbard And Jan Mayen',
+    SZ: 'Swaziland',
+    SE: 'Sweden',
+    CH: 'Switzerland',
+    SY: 'Syrian Arab Republic',
+    TW: 'Taiwan',
+    TJ: 'Tajikistan',
+    TZ: 'Tanzania',
+    TH: 'Thailand',
+    TL: 'Timor-Leste',
+    TG: 'Togo',
+    TK: 'Tokelau',
+    TO: 'Tonga',
+    TT: 'Trinidad And Tobago',
+    TN: 'Tunisia',
+    TR: 'Turkey',
+    TM: 'Turkmenistan',
+    TC: 'Turks And Caicos Islands',
+    TV: 'Tuvalu',
+    UG: 'Uganda',
+    UA: 'Ukraine',
+    AE: 'United Arab Emirates',
+    GB: 'United Kingdom',
+    US: 'United States',
+    UM: 'United States Outlying Islands',
+    UY: 'Uruguay',
+    UZ: 'Uzbekistan',
+    VU: 'Vanuatu',
+    VE: 'Venezuela',
+    VN: 'Viet Nam',
+    VG: 'Virgin Islands, British',
+    VI: 'Virgin Islands, U.S.',
+    WF: 'Wallis And Futuna',
+    EH: 'Western Sahara',
+    YE: 'Yemen',
+    ZM: 'Zambia',
+    ZW: 'Zimbabwe'
+}
+//select elements
+const notificationElement = document.querySelector(".notificationElement");
+
+const cityElement = document.querySelector("#cityElement");
+const countryElement = document.querySelector("#countryElement");
+const degreeElement = document.querySelector("#degreeElement");
+const descriptionElement = document.querySelector("#descriptionElement");
+const iconElement = document.querySelector(".iconElement");
+
+const temperatureElement = document.querySelector("#temperatureElement");
+const pressureElement = document.querySelector("#pressureElement");
+const humidityElement = document.querySelector("#humidityElement");
+const speedElement = document.querySelector("#speedElement");
+const minElement = document.querySelector("#minElement");
+const maxElement = document.querySelector("#maxElement");
+const feelElement = document.querySelector("#feelElement");
+
+//app data
+const weather = {}
+
+//temp units
+weather.temperature = {
+    unit: "celcius"
+}
+
+const KELVIN = 273;
+
+const key = "5c272608dec78c2459567287ca890ac1";
+
+if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(setPosition, showError);
+}
+else {
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = "<p>Browser doesn't support GeoLocation</p>";
+
+}
+
+//show users position
+function setPosition(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    console.log(latitude);
+    console.log(longitude);
+    getWeather(latitude, longitude);
+}
+
+//show error
+function showError(error) {
+    notificationElement.style.display = "block";
+    notificationElement.innerHTML = `<h1 style="color:red;"><center>${error.message}</center></h1>`;
+}
+
+function getWeather(latitude, longitude) {
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+
+    fetch(api)
+        .then(function (response) {
+            let data = response.json();
+            return data;
+        })
+        .then(function (data) {
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.pressure = data.main.pressure;
+            weather.humidity = data.main.humidity;
+            weather.speed = data.wind.speed;
+            weather.min = Math.floor(data.main.temp_min - KELVIN);
+            weather.max = Math.floor(data.main.temp_max - KELVIN);
+            weather.feel = Math.floor(data.main.feels_like - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.icon = data.weather[0].icon;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = countries[data.sys.country];
+        })
+        .then(function () {
+            displayWeather();
+        })
+}
+
+function displayWeather() {
+    console.log(weather);
+    cityElement.innerHTML = `${weather.city}`;
+    countryElement.innerHTML = `${weather.country}`;
+    degreeElement.innerHTML = `${weather.temperature.value}<span>°C</span>`;
+    descriptionElement.innerHTML = `${weather.description}`;
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png" style="width:100px;height:100px"/>`
+
+    temperatureElement.innerHTML = `${weather.temperature.value}<span>°C</span>`;
+    pressureElement.innerHTML = `${weather.pressure}<span> hPa</span>`;
+    humidityElement.innerHTML = `${weather.humidity}<span>%</span>`;
+    speedElement.innerHTML = `${weather.speed}<span>m/s</span>`;
+    minElement.innerHTML = `${weather.min}<span>°C</span>`;
+    maxElement.innerHTML = `${weather.max}<span>°C</span>`;
+    feelElement.innerHTML = `${weather.feel}<span>°C</span>`;
+
+}
+
+function getLocationWeather(){
+    console.log("function called");
+    let city = document.getElementById("CITYNAME").value;
+    console.log(city);
+
+    if(city==""){
+        alert("Enter a city");
+    }
+    else{
+        let api = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
+
+    fetch(api)
+        .then(function (response) {
+            let data = response.json();
+            return data;
+        })
+        .then(function (data) {
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].icon;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function () {
+            displayWeather();
+        })
+    }
+}
